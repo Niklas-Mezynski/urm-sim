fb6ad33f628e4a36aa3cc3ec121bd0b1a16d0c94
Niklas-Mezynski <70883487+Niklas-Mezynski@users.noreply.github.com>
2024-11-17 18:50:13 +0100

    Add CLI options

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,38 @@
-mod instructions;
-mod parser;
-mod simulator;
+use clap::Parser;
+
+pub mod instructions;
+pub mod parser;
+pub mod simulator;
+
+/// URM code parser and interpreter
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Filename or path to the URM program
+    #[arg(index = 1)]
+    file: String,
+
+    /// Values for the input registers
+    #[arg(index = 2)]
+    inputs: Vec<usize>,
+}
 
 fn main() {
-    let urm_code = r#"
-        in(R1, R2)
-        if R2 == 0 goto 5;
-        R2--;
-        R1++;
-        goto 1;
-        out(R1)
-        "#;
+    let args = Args::parse();
 
+    // Read the URM code from the file
+    let urm_code = match std::fs::read_to_string(&args.file) {
+        Ok(urm_code) => urm_code,
+        Err(e) => {
+            eprintln!("Failed to read input file: {}", e);
+            std::process::exit(1);
+        }
+    };
+
+    parse_and_execute(urm_code.as_str(), args.inputs);
+}
+
+fn parse_and_execute(urm_code: &str, input: Vec<usize>) {
     let program = match parser::parse_urm_code(urm_code) {
         Ok(program) => program,
         Err(e) => {
@@ -20,7 +41,7 @@ fn main() {
         }
     };
 
-    let program_result = simulator::simulate_urm(&program, vec![5, 3]);
+    let program_result = simulator::simulate_urm(&program, input);
 
     println!("Program result: {}", program_result);
 }
